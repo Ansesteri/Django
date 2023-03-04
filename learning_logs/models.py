2c9faa379574603af9a50f26e666be888eeb0c53
Ansesteri <angrylemon5678@gmail.com>
2023-03-04 14:10:34 +0200

    Add Homepage

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -19,4 +19,4 @@ class Entry(models.Model):
     class Meta:
         verbose_name_plural = "entries"
     def __str__(self):
-            return f'{self.text[:50]}...'
+        return f'{self.text[:50]}...'
