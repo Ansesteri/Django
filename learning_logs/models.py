66360a79a3d3f029c42261473906df2751af83c9
Ansesteri <angrylemon5678@gmail.com>
2023-03-11 14:09:50 +0200

    Add topic.html

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -14,7 +14,7 @@ class Entry(models.Model):
     
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
-    dete_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = "entries"
